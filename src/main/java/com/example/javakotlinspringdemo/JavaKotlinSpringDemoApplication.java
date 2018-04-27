e9515f12dc81381ddf2974f8a0098c59c43c91b4
Talerngpong Virojwutikul <talerngpong@hotmail.com>
2018-04-27 14:58:31 +0700

    initialize project

---
+++ src/main/java/com/example/javakotlinspringdemo/JavaKotlinSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.javakotlinspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKotlinSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKotlinSpringDemoApplication.class, args);
+	}
+}
